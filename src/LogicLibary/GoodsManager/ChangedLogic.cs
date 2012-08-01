d34d0bedbfb1509c7ff67422f90a65f85d3d98fb
Irvin.Xue <eavia@hotmail.com>
2012-08-01 09:54:46 +0800

    Rev .2

---
+++ src/LogicLibary/GoodsManager/ChangedLogic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LogicLibary.GoodsManager
+{
+    public class ChangedLogic : BaseLogic
+    {
+        private ChangedLogic() { }
+
+        public ChangedLogic(string userkey) : base(userkey) { }
+    }
+}
