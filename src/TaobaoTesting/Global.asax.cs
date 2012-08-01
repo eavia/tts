8c167d3b8e97e16f04fb74e6e5e78044488f76a4
Irvin.Xue <eavia@hotmail.com>
2012-08-01 09:27:19 +0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src/LogicLibary/GoodsManager/GoodsLogic.cs
    	src/TaobaoTesting.suo
    	src/TaobaoTesting/TaobaoTesting.csproj.user

---
+++ src/TaobaoTesting/Global.asax.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace TaobaoTesting
+{
+    public class Global : System.Web.HttpApplication
+    {
+        static string _appKey;
+
+        public static string AppKey
+        {
+            get { return Global._appKey; }
+        }
+        static string _appSecret;
+
+        public static string AppSecret
+        {
+            get { return Global._appSecret; }
+        }
+        static string _apiAddress;
+
+        public static string ApiAddress
+        {
+            get { return Global._apiAddress; }
+        }
+
+        void Application_Start(object sender, EventArgs e)
+        {
+            // 在应用程序启动时运行的代码
+            _appKey=System.Configuration.ConfigurationManager.AppSettings["appKey"];
+            _appSecret = System.Configuration.ConfigurationManager.AppSettings["appSecret"];
+            _apiAddress = System.Configuration.ConfigurationManager.AppSettings["apiAddress"]; 
+        }
+
+        void Application_End(object sender, EventArgs e)
+        {
+            //  在应用程序关闭时运行的代码
+
+        }
+
+        void Application_Error(object sender, EventArgs e)
+        {
+            // 在出现未处理的错误时运行的代码
+
+        }
+
+        void Session_Start(object sender, EventArgs e)
+        {
+            // 在新会话启动时运行的代码
+        }
+
+        void Session_End(object sender, EventArgs e)
+        {
+            // 在会话结束时运行的代码。 
+            // 注意: 只有在 Web.config 文件中的 sessionstate 模式设置为
+            // InProc 时，才会引发 Session_End 事件。如果会话模式设置为 StateServer 
+            // 或 SQLServer，则不会引发该事件。
+
+        }
+
+    }
+}
---
+++ src/TaobaoTesting/GoodsManager/BrandList.aspx.designer.cs
@@ -0,0 +1,96 @@
+﻿//------------------------------------------------------------------------------
+// <自动生成>
+//     此代码由工具生成。
+//
+//     对此文件的更改可能会导致不正确的行为，并且如果
+//     重新生成代码，这些更改将会丢失。 
+// </自动生成>
+//------------------------------------------------------------------------------
+
+namespace TaobaoTesting.GoodsManager {
+    
+    
+    public partial class BrandList {
+        
+        /// <summary>
+        /// trvBrand 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TreeView trvBrand;
+        
+        /// <summary>
+        /// pelEditor 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel pelEditor;
+        
+        /// <summary>
+        /// Label1 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+        
+        /// <summary>
+        /// txtName 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtName;
+        
+        /// <summary>
+        /// btnAddNode 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAddNode;
+        
+        /// <summary>
+        /// btnDelete 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnDelete;
+        
+        /// <summary>
+        /// pelList 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel pelList;
+        
+        /// <summary>
+        /// dlGoods 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DataList dlGoods;
+        
+        /// <summary>
+        /// pgrBrandList 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::Wuqi.Webdiyer.AspNetPager pgrBrandList;
+    }
+}
